7ae8d51de6d9dd349a385d525590a97c141d1f72
lirik <lirik.rexhepi@student.uni-pr.edu>
2026-05-16 16:35:40 +0200

    RSA key handling
    Creates private and public key for the server file.
    Imports needed libraries for the development of the server
    Creates 2 new functions encrypt and decrypt messages

---
+++ server.py
@@ -0,0 +1,43 @@
+import socket
+import threading
+
+from cli_interface import ServerCli
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
+
+server_private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+server_public_key = server_private_key.public_key()
+
+
+def public_key_to_bytes(public_key):
+    return public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+
+def bytes_to_public_key(key_bytes):
+    return serialization.load_pem_public_key(key_bytes)
+
+
+def encrypt_message(message, public_key):
+    return public_key.encrypt(
+        message.encode(),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+
+def decrypt_message(encrypted_message):
+    decrypted = server_private_key.decrypt(
+        encrypted_message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return decrypted.decode()
